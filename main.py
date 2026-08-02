2bd7c5c0b3c361f3b259a9732d81dd177bb2cde4
Ronak Jawalia <ronakjawaliya@gmail.com>
2026-08-03 01:37:17 +0530

    Improved the functioning of code

--- main.py
+++ main.py
@@ -5,17 +5,27 @@ from getpass import getpass
 
 
 def master_password_exists():
-    return os.path.exists("master_password.json")
+    if not os.path.exists("master_password.json"):
+        return False
+
+    try:
+        with open("master_password.json", "r") as f:
+            data = json.load(f)
+
+        return "master_password" in data
+
+    except json.JSONDecodeError:
+        return False
 
 def create_master_password():
 
     while True:
-       password = getpass("Enter master password: ").strip()
+       password = input("Enter master password: ").strip()
        if not password:
            print("Please enter a master password")
            continue
 
-       confirm_password = getpass("Confirm master password: ").strip()
+       confirm_password = input("Confirm master password: ").strip()
        if not confirm_password:
            print("Please enter a master password")
            continue
@@ -50,13 +60,17 @@ def login():
         print("Master password file is corrupted.")
         return False
 
+    if "master_password" not in data:
+        print("Invalid master password file.")
+        return False
+
     stored_password = data["master_password"]
 
     attempts = 3
 
     while attempts > 0:
 
-        password = getpass("Enter master password: ").strip()
+        password = input("Enter master password: ").strip()
         if not password:
             attempts -= 1
             print("Please enter a master password")
@@ -82,16 +96,83 @@ def load_passwords():
             return json.load(f)
 
     except FileNotFoundError:
-        print("Master password file not found.")
+        print("Password file not found. Creating a new vault.")
         return []
 
     except json.JSONDecodeError:
-        print("Master password file is corrupted.")
+        print("Password file is corrupted.")
         return []
 
 def save_passwords(passwords):
     with open("passwords.json", "w") as f:
         json.dump(passwords, f, indent=4)
+
+def add_password():
+
+    passwords = load_passwords()
+
+    while True:
+        try:
+            password_id = int(input("Enter ID: "))
+
+        except ValueError:
+            print("ID must be an integer.")
+            continue
+
+        if password_id <= 0:
+            print("ID must be +ve integer.")
+            continue
+
+        duplicate_found = False
+
+        for existing_password in passwords:
+            if existing_password["id"] == password_id:
+                duplicate_found = True
+                break
+
+        if duplicate_found:
+            print("ID already exists. Enter another ID.")
+            continue
+
+        break
+
+    while True:
+        website = input("Enter website: ").strip()
+
+        if not website:
+            print("Please enter a website.")
+            continue
+
+        break
+
+    while True:
+        username = input("Enter username: ").strip()
+
+        if not username:
+            print("Please enter a username.")
+            continue
+
+        break
+
+    while True:
+        password = input("Enter master password: ").strip()
+
+        if not password:
+            print("Please enter a password.")
+            continue
+
+        break
+
+    password_entry = {
+        "id": password_id,
+        "website": website,
+        "username": username,
+        "password": password
+    }
+
+    passwords.append(password_entry)
+    save_passwords(passwords)
+    print("Password added successfully.")
 
 
 
@@ -104,4 +185,6 @@ if login():
 else:
     print("Too many failed login attempts.")
     exit(0)
+
+
 
